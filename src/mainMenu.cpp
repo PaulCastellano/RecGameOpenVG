f1e261a2e553f0dbfb5099d7b540d65c91e8d98f
Andrei Miron <turelsminion19@gmail.com>
2019-11-20 21:32:21 +0200

    Game v5

--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -5,6 +5,6 @@ void Game::mainMenu()
     WindowClear();
     Start(this->windowWidth, this->windowHeight);
     // std::cout << this->windowWidth << " " << this->windowHeight << std::endl;
-    Image(0, 0, 1, 1, "/home/pi/Desktop/RecGameOpenVG/assets/mainMenu.jpeg");
+    Image(0, 0, 100, 100, "/home/pi/Desktop/RecGameOpenVG/assets/mainMenu.jpeg");
     End();
 }
